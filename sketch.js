04649632ecfeb04122140a975cc08c32b7dce724
teacherwhj <whjr.steacher@gmail.com>
2021-01-18 01:28:34 +0530

    added constraint

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var bg;
+var platform;
+var constrainedlog;
+
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    bg=loadImage('sprites/bg.png');
 }
 
 function setup(){
@@ -15,10 +19,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
+    
+    ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,300,200,280);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -31,15 +34,17 @@ function setup(){
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    log4 = new Log(760,120,150, PI/6);
+    log5 = new Log(870,120,150, -PI/6);
 
     bird = new Bird(100,100);
-
+    constrainedlog = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body, constrainedlog.body);
+  
 }
 
 function draw(){
-    background(backgroundImg);
+    background(bg);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -58,7 +63,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
-    bird.display();
     platform.display();
+    bird.display();
+    constrainedlog.display();
+    chain.display();
+   
 }
